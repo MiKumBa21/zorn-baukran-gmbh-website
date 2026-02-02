6d9cf69ae189e56d72a59ec58a0340ac562bfb22
Matthias <matthias.brueckl22@gmail.com>
2026-02-02 19:37:19 +0100

    Button Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ConstCranes from './Pages/Cranes/ConstCranes/ConstCranes';
-import Contact from './Pages/Contact/Contact';
-import Cranes from './Pages/Cranes/Cranes';
-import Eurogru from './Pages/Eurogru/Eurogru';
-import Home from './Pages/Home/Home';
-import MobilCranes from './Pages/Cranes/MobilCranes/MobilCranes';
-import QuickErectCranes from './Pages/Cranes/QuickErectCranes/QuickErectCranes';
-import UsedCranes from './Pages/UsedCranes/UsedCranes';
-import NotFoundPage from './Pages/NotFoundPage/NotFoundPage';
+import ConstCranes from './pages/Cranes/ConstCranes/ConstCranes';
+import Contact from './pages/Contact/Contact';
+import Cranes from './pages/Cranes/Cranes';
+import Eurogru from './pages/Eurogru/Eurogru';
+import Home from './pages/Home/Home';
+import MobilCranes from './pages/Cranes/MobilCranes/MobilCranes';
+import QuickErectCranes from './pages/Cranes/QuickErectCranes/QuickErectCranes';
+import UsedCranes from './pages/UsedCranes/UsedCranes';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 
 const router = createBrowserRouter([
